d2b87f6f5d253f17107963d26e503ca72dee7c8c
rzhasibuan <rzhasibuan@gmail.com>
2021-04-21 22:22:03 +0700

    Random qoute finish

--- get-quote.py
+++ get-quote.py
@@ -1,16 +1,18 @@
 import random
 def cool():
-  print("Keep it logically awesome.")
+  # print("Keep it logically awesome.")
 
-  f = open("quotes.txt")
-  quotes = f.readlines()
-  f.close()
+  f = open("quotes.txt");
+  quotes = f.readlines();
+  f.close();
 
  
-  last = 13
+  last = 13;
   rnd = random.randint(0,last);
+  rnd2 = random.randint(0,last-1);
   # print(quotes[rnd])
-  print(quotes[rnd])
+  print(quotes[rnd]);
+  print(quotes[rnd2]);
 
 if __name__== "__main__":
-  cool()
+  cool();
